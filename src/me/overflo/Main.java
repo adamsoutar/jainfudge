67cc685aa2bc6fed7a26537ccc2ded2fcab7252e
Adam Soutar <adam@overflo.me>
2021-06-19 23:27:46 +0100

    Read input from a file

--- src/me/overflo/Main.java
+++ src/me/overflo/Main.java
@@ -1,9 +1,16 @@
 package me.overflo;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        var rawCode = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
+        var inputBytes = Files.readAllBytes(
+                Paths.get("./input.bf").toAbsolutePath()
+        );
+        var rawCode = new String(inputBytes);
+
         var code = Instructions.FromCode(rawCode);
         var vm = new VM(code);
         vm.Run();
